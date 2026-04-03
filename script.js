772857690461404a6bb694605dbeeb7f6a0b1ef8
Akshit Rajput <akshitrajput167@gmail.com>
2026-04-03 19:52:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message sent successfully!");
+});
